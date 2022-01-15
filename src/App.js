c8eb22ef2bdfbd038888003a488ae77960552543
zahra-p <zahra.adelinia@gmail.com>
2022-01-15 17:58:22 +0330

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo2.png';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+
