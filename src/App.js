0ae0428b995d986c41e2d367a9716f765d695fd2
zahra-p <zahra.adelinia@gmail.com>
2022-01-16 21:33:57 +0330

    Dictionary component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo2.png';
 import './App.css';
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+             Coded by Zahra Adelinia
+          </small>
+          
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,26 @@
+import React,{useState} from "react";
+import "./Dictionary.css"
+
+export  default function Dictionary(){
+    let [keyword, setKeyword] =useState("");
+
+    function search(event){
+        event.preventDefault();
+        alert(`Searching for ${keyword} definition`);
+    }
+
+    function handlekeywordchange(event){
+        console.log(event.target.value);
+        setKeyword(event.target.value);
+
+    }
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" autoFocus={true} onChange={handlekeywordchange}/>
+
+            </form>
+            
+        </div>
+    );
+}
