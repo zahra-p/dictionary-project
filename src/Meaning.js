05ede049365ba61a4249e05c5cdbe3ad1669077a
zahra-p <zahra.adelinia@gmail.com>
2022-01-18 11:45:32 +0330

    Synonyms component compeleted

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Synonyms from "./Synonyms";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props){
-    console.log(props.meaning)
+    
     return (
     <div className="Meaning">
-        <h3>{props.meaning.partOfSpeech}</h3>;
+        <h3>{props.meaning.partOfSpeech}</h3>
         {props.meaning.definitions.map(function (definition , index){
             return(
                 <div key={index}>
