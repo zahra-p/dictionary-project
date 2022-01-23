df9203bf16b26182080cdff2e29aa0519c10a7f5
zahra-p <zahra.adelinia@gmail.com>
2022-01-23 13:44:58 +0330

    Photos component completed

--- src/App.js
+++ src/App.js
@@ -18,6 +18,14 @@ export default function App() {
           </small>
           
         </footer>
+
+        <footer className="App-footer">
+          <small>
+              Coded by Zahra Adelinia and is <a href="https://github.com/zahra-p/dictionary-project" 
+              target="_blank" rel="noopener noreferrer">open-sourced on GitHub</a> and 
+              <a href="https://wonderful-sammet-14c71a.netlify.app/" target="_blank" rel="noopener noreferrer">hosted on Netlify</a>
+          </small>
+        </footer>
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React,{useState} from "react";
 import Results from "./Results.js";
+import Photos from "./Photos.js";
 import "./Dictionary.css";
 
 
@@ -8,15 +9,14 @@ export  default function Dictionary(props){
     let [keyword, setKeyword] =useState(props.defaultKeyword);
     let [results, setResults]= useState(null);
     let [loaded, setLoaded]= useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function handleDictionaryResponse(response){
         setResults(response.data[0]);
     }
 
     function handlePexelsResponse(response){
-        console.log("handlePexelsResponse");
-        console.log(response);
-
+        setPhotos(response.data.photos);
     }
 
     function search(){
@@ -25,7 +25,7 @@ export  default function Dictionary(props){
         axios.get(apiUrl).then(handleDictionaryResponse);
 
         let pexelsApiKey="563492ad6f91700001000001bb742491a94f4e7592162743fcc74e69";
-        let pexelsApiUrl =`https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+        let pexelsApiUrl =`https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
         
         let headers = {Authorization: `Bearer ${pexelsApiKey}`};
         axios.get(pexelsApiUrl, { headers: headers}).then(handlePexelsResponse);
@@ -59,6 +59,7 @@ export  default function Dictionary(props){
                     </div>
                 </section>  
                     <Results results={results} />
+                    <Photos photos={photos} />
                     
                 </div>
             );
